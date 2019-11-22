a673f643d97e721b88386ff3b27611896f2efeb6
Geovani Benita <36940541+Geobm@users.noreply.github.com>
2019-11-22 12:08:00 -0600

    Update Nano.py

--- Nano.py
+++ Nano.py
@@ -1,90 +1,73 @@
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
-from time import sleep
 import datetime
 
-
-def database():
-	username = Username.get()
-	passw = password.get()
- 	sector = area.get()
- 	prog = var1.get()
-	time = datetime.datetime.now()
-	conn = sqlite3.connect('Nanotechnology.db')
-	with conn:
-		cursor=conn.cursor()
-	cursor.execute('CREATE TABLE IF NOT EXISTS Student (Username TEXT,Name TEXT,Area TEXT,Programs INT,Fecha TEXT)')
-	cursor.execute('INSERT INTO Student (Username,Name,Area,Programs,Fecha) VALUES(?,?,?,?,?)',(username,passw,sector,prog,time))
-	conn.commit()
-	sleep(1)
-	Label(root, text= "Succesful Registration", fg="green", font =("bold",12)).place(x=180, y=450)
- 	root.destroy() 
+init = Tk()
+init.geometry('500x500')
+init.title("Nanotechnology Lab Registration")
+#init.configure(background = 'white')
 
 
-def main():
-	Admin = admin.get()
-	if (Admin == "oxana"):
-		login.destroy()
-		root= Tk()
-		root.geometry('500x500')
-		root.title("Nanotechnology Lab Registration")
-		label_0 = Label(root, text="Nanotechnology \nLab Registration",width=20,font=("bold", 20))
-		label_0.place(x=90,y=34)
-
-
-		label_1 = Label(root, text="ID: ",width=20,font=("bold", 10))
-		label_1.place(x=80,y=130)
-
-		entry_1 = Entry(root,textvar=Username)
-		entry_1.place(x=240,y=130)
-
-		label_2 = Label(root, text="Name",width=20,font=("bold", 10))
-		label_2.place(x=80,y=180)
-
-		entry_2 = Entry(root,textvar=password)
-		entry_2.place(x=240,y=180)
-		entry_2.config(show="*")
-
-
-		label_4 = Label(root, text="Section ",width=20,font=("bold", 10))
-		label_4.place(x=80,y=250)
-
-		list1 = ['Undergraduate','Postgraduate','Other'];
-
-		droplist=OptionMenu(root,area, *list1)
-		droplist.config(width=15)
-		area.set('Select your area:') 
-		droplist.place(x=240,y=250)
-
-		label_4 = Label(root, text="Usage: ",width=20,font=("bold", 10))
-		label_4.place(x=85,y=330)
-
-		Checkbutton(root, text="IR spectroscopy", variable=var1).place(x=235,y=330)
-		Checkbutton(root, text="Other programs", variable=var2).place(x=235,y=350)
-
-		Button(root, text='Submit',width=20,bg='brown',fg='white',command=database).place(x=180,y=380)
-	else: 
-		messagebox.showinfo("Error", "Incorrect Password")
-
-
-
-
-login = Tk()
 Username = StringVar()
 area = StringVar()
-password = StringVar()
+name = StringVar()
 var1 = IntVar()
 var2 = IntVar()
-time = 0
 admin = StringVar()
-login.geometry('250x250')
-login.title('Login')
-label_5 = Label(login, text = "Enter admin \npassword:",width=12,font=("bold", 10))
-label_5.place(x= 40, y = 100)
-admin_entry = Entry(login, textvar = admin)
-admin_entry.place(x= 60, y = 150)
-admin_entry.config(show = "*")
-Button(login, text='Ok',width=10,bg='brown',fg='white',command=main).place(x=80,y=190)
 
-login.mainloop()
+def database():
+   username = Username.get()
+   Name = name.get()
+   sector = area.get()
+   prog = var1.get()
+   if (username == '' or Name == '' or sector == ''):
+      messagebox.showinfo("!", "Empty value\nTry again")   
+   else : 
+      time = datetime.datetime.now()
+      conn = sqlite3.connect('Nano.db')
+      with conn:
+         cursor=conn.cursor()
+      cursor.execute('CREATE TABLE IF NOT EXISTS Student (ID TEXT,Name TEXT,Area TEXT,Programs INT,Fecha TEXT)')
+      cursor.execute('INSERT INTO Student (ID,Name,Area,Programs,Fecha) VALUES(?,?,?,?,?)',(username,Name,sector,prog,time))
+      conn.commit()
+      messagebox.showinfo("OK", "Registration\n Succesfully")
+      exit()
+
+label_0 = Label(init, text="Nanotechnology \nLab Registration",width=20,font=("bold", 20))
+label_0.place(x=90,y=34)
+
+
+label_1 = Label(init, text="ID: ",width=20,font=("bold", 10))
+label_1.place(x=80,y=130)
+
+entry_1 = Entry(init,textvar=Username)
+entry_1.place(x=240,y=130)
+
+label_2 = Label(init, text="Name(s):",width=20,font=("bold", 10))
+label_2.place(x=80,y=180)
+
+entry_2 = Entry(init,textvar=name)
+entry_2.place(x=240,y=180)
+
+
+label_4 = Label(init, text="Section: ",width=20,font=("bold", 10))
+label_4.place(x=80,y=250)
+
+list1 = ['Undergraduate','Postgraduate','Other'];
+
+droplist=OptionMenu(init,area, *list1)
+droplist.config(width=15)
+area.set('Select your area:') 
+droplist.place(x=240,y=250)
+
+label_4 = Label(init, text="Usage: ",width=20,font=("bold", 10))
+label_4.place(x=85,y=330)
+
+Checkbutton(init, text="IR spectroscopy", variable=var1).place(x=235,y=330)
+Checkbutton(init, text="Other programs", variable=var2).place(x=235,y=350)
+
+Button(init, text='Submit',width=20,bg='grey',fg='white',command=database).place(bordermode = OUTSIDE, x=180, y=400)
+
+init.mainloop()
+
